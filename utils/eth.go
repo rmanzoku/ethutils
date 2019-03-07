d973bf5c3a836f67b14fa66cb28c1147e5ffa731
Ryo Manzoku <manzoku.ryo@gmail.com>
2019-03-07 05:59:15 +0000

    ToWei

--- utils/eth.go
+++ utils/eth.go
@@ -1,10 +1,15 @@
 package utils
 
 import (
+	"bytes"
 	"context"
+	"io/ioutil"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
 )
 
 func NewEthClient(rpc string) (*ethclient.Client, error) {
@@ -21,9 +26,34 @@ func NewEthClient(rpc string) (*ethclient.Client, error) {
 	return conn, nil
 }
 
+func NewTransactor(keystore, passphrase string) (*bind.TransactOpts, error) {
+	key, err := ioutil.ReadFile(keystore)
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewTransactor(bytes.NewReader(key), passphrase)
+}
+
 func ToEther(wei *big.Int) *big.Float {
 	ether, _ := new(big.Float).SetString("1000000000000000000")
 
 	w := new(big.Float).SetInt(wei)
 	return new(big.Float).Quo(w, ether)
+}
+
+func ToWei(ether *big.Float) (*big.Int, error) {
+	etherDecimal, err := decimal.NewFromString(ether.String())
+	if err != nil {
+		return nil, err
+	}
+
+	baseWei, _ := decimal.NewFromString("1000000000000000000")
+
+	retDecimal := etherDecimal.Mul(baseWei)
+	ret, ok := new(big.Int).SetString(retDecimal.String(), 10)
+	if !ok {
+		return nil, errors.New("Invalit number")
+	}
+
+	return ret, nil
 }
